38fd943e71d4cf7bf8c77dd0024f3e44a5e5bce5
Brennan Bradley <Brennanb572@gmail.com>
2023-05-13 15:05:58 -0700

    background video added

---
+++ index.js
@@ -0,0 +1,16 @@
+// 1. Declaring variables 
+
+const themeparks = document.querySelector('#themeparks');
+const restaurants = document.querySelector('#restaurants');
+const hotels = document.querySelector('#hotels');
+
+// 2. DOM manipulation for themeparks
+
+
+
+
+// 3. DOM manipulation for restaurants
+
+
+
+// 4. DOM manipulation for hotels
