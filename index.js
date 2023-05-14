d8650aa2534116e8f1e09aa576ab0cd6e29a2be1
Brennan Bradley <Brennanb572@gmail.com>
2023-05-14 08:49:04 -0700

    several changes to css file with added data

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 function renderOneHotel(hotel){
     let hotelCard = document.createElement('li')
-    hotelCard.className = 'card'
+    hotelCard.className = 'hotelcard'
     hotelCard.innerHTML =`
-    <img src="${hotel.imageURL}">
+    <img src="${hotel.imageUrl}">
     <div class="content">
     <h4>${hotel.name}</h4>
     <p>${hotel.address}</p>
     <p>${hotel.promo}</p>
     <p>
-      Likes:<span class="amount-of-likes">${hotel.likes}</span>
+      Likes: <span class="amount-of-likes">${hotel.likes}</span>
     </p>
     <p>${hotel.address}</p>
+    </div>
     `
     document.querySelector('#hotels').appendChild(hotelCard)
 }
