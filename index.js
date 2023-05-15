d0bd56d6f468928d7ca773ed3e4e3b9c7e79d485
BrennanB572 <Brennanb572@gmail.com>
2023-05-15 10:41:11 -0700

    additional functionality was added to the buttons and also the custom API was built and added to the backend for communication to the front end, mostly with postman

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 document.querySelector('#hotel-filler').addEventListener('submit', handleSubmit)
- 
- 
- 
- 
- 
- function renderOneHotel(hotel){
+
+function handleSubmit(event){
+  event.preventDefault()
+  let hotelObj = {
+    name:event.target.name.value,
+    imageUrl:event.target.image_url.value,
+    address:event.target.address.value,
+    promo:event.target.promo.value
+  }
+  renderOneHotel(hotelObj)
+}
+
+function renderOneHotel(hotel){
     let hotelCard = document.createElement('li')
     hotelCard.className = 'hotelcard'
     hotelCard.innerHTML =`
-    <img src="${hotel.imageUrl}">
+    <img width="300px" height="200px"src="${hotel.imageUrl}">
     <div class="content">
     <h4>${hotel.name}</h4>
     <p>${hotel.address}</p>
     <p>${hotel.promo}</p>
-    <p>
-      Likes: <span class="amount-of-likes">${hotel.likes}</span>
-    </p>
-    <p>${hotel.address}</p>
+    <button id='remove'>remove</button>
     </div>
     `
+    hotelCard.querySelector('#remove').addEventListener('click', () => {
+      hotelCard.remove()
+      deleteHotel(hotel.id)
+    })
+    
     document.querySelector('#hotels').appendChild(hotelCard)
 }
 
@@ -26,6 +35,27 @@ function getHotels(){
   fetch('http://localhost:3000/hotels')
   .then(res => res.json())
   .then(hotels => hotels.forEach(hotel => renderOneHotel(hotel)))
+}
+
+function submitHotel(hotelObj){
+  fetch('http://localhost:3000/hotels',{
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(hotelObj)
+  })
+  .then(res => res.json())
+}
+
+function deleteHotel(id){
+  fetch(`http://localhost:3000/hotels/${id}`,{
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
 }
 
 function initialize(){
